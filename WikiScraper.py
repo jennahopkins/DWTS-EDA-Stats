2b011c0d555accf78d4b7a324fc852ed2f008b04
jennahopkins <jhopkins2@willamette.edu>
2025-10-19 10:31:09 -0700

    fixed data scraping and writing issue

--- WikiScraper.py
+++ WikiScraper.py
@@ -39,6 +39,7 @@ for k, table in enumerate(tables):
         couple = col_text[0]
         scores = col_text[1].split(" ")
         scores = [score.strip("(,)") for score in scores]
+        scores = [score.strip(")[a]") for score in scores]
         if scores == [""]:
             data_dict.pop(week)
             break
@@ -64,17 +65,17 @@ for week_num, couples in data_dict.items():
         break
 
 for week_num, couples in data_dict.items():
-    judge_scores = {}
     for couple, scores in couples.items():
+        judge_scores = {}
         for i, judge in enumerate(judges[week_num]):
             if judge not in judge_names[0]:
                 judge = "Guest Judge"
             judge_scores[judge] = scores[i + 1]
-        data_dict[week_num][couple] = judge_scores
+        data_dict[week_num][couple] = judge_scores.copy()
 
 with open('JudgeScores.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Week", "Couple", judge_names[0][0] + " Score", judge_names[0][1] + " Score", judge_names[0][2] + " Score", "Guest Judge Score"]) # Write headers
     for week, couples in data_dict.items():
         for couple, scores in couples.items():
-            writer.writerow([week, couple, scores.get(judge_names[0][0], "N/A"), scores.get(judge_names[0][1], "N/A"), scores.get(judge_names[0][2], "N/A"), scores.get("Guest Judge", "N/A")])
+            writer.writerow([week, couple, scores.get(judge_names[0][0], None), scores.get(judge_names[0][1], "N/A"), scores.get(judge_names[0][2], "N/A"), scores.get("Guest Judge", "N/A")])
